8c2bdb0269f9b57de3dd04d4c15a9396812f37fc
natusina <45242970+natusina@users.noreply.github.com>
2021-03-24 12:04:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -94,6 +94,9 @@ def get_suggests(user_id):
     return suggests
 
 
+'''if __name__ == '__main__':
+    app.run()'''
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
